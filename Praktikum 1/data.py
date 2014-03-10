55aa5897973b533244f68351fc325df3d2ef0916
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-03-10 16:42:23 +0700

    Modifikasi pada __init__ di data.py, kurang "self" nya

--- Praktikum 1/data.py
+++ Praktikum 1/data.py
@@ -1,12 +1,12 @@
 import time
 class DataRequest(self):
-	def ___init__(action, variable):
+	def ___init__(self, action, variable):
 		self.action = action
 		self.variable = variable
 		self.timestamp = int(time.time())
 
 class DataResponse(self):
-	def __init__(reqAction, reqVariable, result):
+	def __init__(self, reqAction, reqVariable, result):
 		self.reqAction = action
 		self.reqVariable = variable
 		self.timestamp = int(time.time())
