e6ab697a30f1f46e50475b7e17a7e48da906af64
Ali <ali.ariff12@gmail.com>
2014-03-11 09:25:46 +0700

    progress

--- Praktikum 1/klien1.py
+++ Praktikum 1/klien1.py
@@ -10,8 +10,8 @@ client_socket.connect(('localhost', 5000))
  
 # send message to server
 #req = DataRequest('requestWeatherDateSpecific', ['1394623007'])
-#req = DataRequest('requestWeatherDateRange', ['1394536607', '1394709407'])
-req = DataRequest('requestWeatherAll', ['1394536607', '1394882207'])
+req = DataRequest('requestWeatherDateRange', ['1394536607', '1394709407'])
+#req = DataRequest('requestWeatherAll', ['1394536607', '1394882207'])
 
 client_socket.send(json.dumps(req.__dict__))
 
--- Praktikum 1/server1.py
+++ Praktikum 1/server1.py
@@ -1,6 +1,7 @@
 # import required module
-import socket, select, sys, json, time, string
+import socket, select, sys, json, time, string, datetime
 from data import DataRequest, DataResponse
+from datetime import date
 
 # creating socket server object, bind, and listen
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,12 +12,18 @@ server_socket.listen(5)
 input_list = [server_socket]
 
 # open file 
+list_bulan = ['Januari', 'Februari', 'Maret', 'April',
+		'Mei', 'Juni', 'Juli', 'Agustus', 'September',
+		'Oktober', 'November', 'Desember']
 f = open('data.txt', 'r')
-lines = []
+lines = {}
 for line in f:
-	lines.append(string.split(line, ' - ')[1][:-1])
+	temp = string.split(line, ' - ')
+	temp[0] = string.split(temp[0], ', ')[1]
+	temp[1] = temp[1][:-1]
+	lines[temp[0]] = temp[1]
 f.close()
-
+print lines
 try:
 	while 1:
 		# serving multiple client alternately; one socket in a time
@@ -39,17 +46,39 @@ try:
 					dr = DataResponse(json_data['action'], json_data['variable'], [])
 					print json_data['variable'][0]
 					if (json_data['action'] == 'requestWeatherDateSpecific'):
-						waktu = int(time.strftime('%d', time.localtime(float(json_data['variable'][0]))))
+						tgl = time.strftime('%d', time.localtime(float(json_data['variable'][0])))
+						bulan = int(time.strftime('%m', time.localtime(float(json_data['variable'][0]))))
+						tahun = time.strftime('%Y', time.localtime(float(json_data['variable'][0])))
+						bulan = list_bulan[bulan-1]
+						waktu = tgl + ' ' + bulan + ' ' + tahun
+						print lines[waktu]
 						try:
-							result.append(lines[waktu-11])
+							result.append({json_data['variable'][0]: lines[waktu]})
 						except:
 							result = []
 					elif (json_data['action'] == 'requestWeatherDateRange'):
-						waktu_awal = int(time.strftime('%d', time.localtime(float(json_data['variable'][0]))))
-						waktu_akhir = int(time.strftime('%d', time.localtime(float(json_data['variable'][1]))))
+						tgl_awal = time.strftime('%d', time.localtime(float(json_data['variable'][0])))
+						bulan_awal = int(time.strftime('%m', time.localtime(float(json_data['variable'][0]))))
+						tahun_awal = time.strftime('%Y', time.localtime(float(json_data['variable'][0])))
+						d0 = date(int(tahun_awal), int(bulan_awal), int(tgl_awal))
+						bulan_awal = list_bulan[bulan_awal-1]
+						waktu_awal = tgl_awal + ' ' + bulan_awal + ' ' + tahun_awal
+						
+						tgl_akhir = time.strftime('%d', time.localtime(float(json_data['variable'][1])))
+						bulan_akhir = int(time.strftime('%m', time.localtime(float(json_data['variable'][1]))))
+						tahun_akhir = time.strftime('%Y', time.localtime(float(json_data['variable'][1])))
+						d1 = date(int(tahun_akhir), int(bulan_akhir), int(tgl_akhir))
+						bulan_akhir = list_bulan[bulan_akhir-1]
+						waktu_akhir = tgl_akhir + ' ' + bulan_akhir + ' ' + tahun_akhir
+						print waktu_awal
+						d0 = d0 + datetime.timedelta(days=10)
+						print d0
+						print waktu_akhir
+						delta = d1 - d0
+						print delta.days
 						try:
-							for x in range(waktu_awal-11, waktu_akhir-10):
-								result.append(lines[x])
+							for x in range(0, delta.days + 1):
+								print x
 						except:
 							result = []
 					elif (json_data['action'] == 'requestWeatherAll'):
