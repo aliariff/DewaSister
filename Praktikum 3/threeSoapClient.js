06fda2a0243dc47298eb47b717cbb202e9fbfea5
Ali Ariff <ali.ariff12@gmail.com>
2014-05-25 20:07:25 +0700

    komit call function soap

--- Praktikum 3/threeSoapClient.js
+++ Praktikum 3/threeSoapClient.js
@@ -3,21 +3,55 @@ var express = require('express');
 var http = require('http');
 
 var requestStockQuote = null;
+var requestConversionRate = null;
+var requestCitiesByCountry = null;
+var requestWeather = null;
+
+// Komen kalau gak butuh proxy
+var proxy = { proxy: "http://10.151.34.10:8080", strictSSL: false };
 
 soap.createClient('./soapClientWSDL/stockquote.asmx.xml', function(err, client) {
 	//console.log(client);
 
-	requestStockQuote = function(symbol) {
+	requestStockQuote = function(symbol, callback) {
 		client.GetQuote({'symbol': symbol}, function(err, result, body) {
-			console.log(result);
-		}, {
-			proxy: "http://10.151.34.10:8080",
-			strictSSL: false
-		});
+			callback(result);
+		}, proxy);
 	}
 	
     //console.log();
 });
+
+soap.createClient('./soapClientWSDL/CurrencyConvertor.asmx.xml', function(err, client) {
+	//console.log(client);
+
+	requestConversionRate = function(FromCurrency, ToCurrency, callback) {
+		client.GetConversionRate({'FromCurrency': FromCurrency, 'ToCurrency': ToCurrency}, function(err, result, body) {
+			callback(result);
+		}, proxy);
+	}
+	
+    //console.log();
+});
+
+soap.createClient('./soapClientWSDL/globalweather.asmx.xml', function(err, client) {
+	//console.log(client);
+
+	requestCitiesByCountry = function(CountryName, callback) {
+		client.GetCitiesByCountry({'CountryName': CountryName}, function(err, result, body) {
+			callback(result);
+		}, proxy);
+	}
+	
+	requestWeather = function(CityName, CountryName, callback) {
+		client.GetCitiesByCountry({'CityName': CityName, 'CountryName': CountryName}, function(err, result, body) {
+			callback(result);
+		}, proxy);
+	}
+	
+    //console.log();
+});
+
 
 var app = express();
 app.configure(function() {
@@ -28,6 +62,7 @@ app.configure(function() {
 });
 
 app.get('/stock/:param', function(req, res) {
+	// Cek dulu apakah variabel requestStockQuote apa masih null, kalau iya kasih pesan error dulu (SOAP nya berarti belum ke load pas pertama kali load skrip ini
     res.render('viewStock.ejs')
 });
 
