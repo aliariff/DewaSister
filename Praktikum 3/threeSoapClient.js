b16855627ba2d1676f1e466cb593a7a74975d8fc
Ali Ariff <ali.ariff12@gmail.com>
2014-05-25 22:31:43 +0700

    komit no 2

--- Praktikum 3/threeSoapClient.js
+++ Praktikum 3/threeSoapClient.js
@@ -1,6 +1,7 @@
 var soap = require('soap');
 var express = require('express');
 var http = require('http');
+var xml2js = require("xml2js");
 
 var requestStockQuote = null;
 var requestConversionRate = null;
@@ -15,7 +16,9 @@ soap.createClient('./soapClientWSDL/stockquote.asmx.xml', function(err, client) 
 
 	requestStockQuote = function(symbol, callback) {
 		client.GetQuote({'symbol': symbol}, function(err, result, body) {
-			callback(result);
+			xml2js.parseString(result.GetQuoteResult, {"explicitArray": false }, function(errXml, resultXml) {
+				callback(resultXml.StockQuotes.Stock);
+			});
 		}, proxy);
 	}
 	
@@ -26,8 +29,8 @@ soap.createClient('./soapClientWSDL/CurrencyConvertor.asmx.xml', function(err, c
 	//console.log(client);
 
 	requestConversionRate = function(FromCurrency, ToCurrency, callback) {
-		client.GetConversionRate({'FromCurrency': FromCurrency, 'ToCurrency': ToCurrency}, function(err, result, body) {
-			callback(result);
+		client.ConversionRate({'FromCurrency': FromCurrency, 'ToCurrency': ToCurrency}, function(err, result, body) {
+			callback(result.ConversionRateResult);
 		}, proxy);
 	}
 	
@@ -39,13 +42,30 @@ soap.createClient('./soapClientWSDL/globalweather.asmx.xml', function(err, clien
 
 	requestCitiesByCountry = function(CountryName, callback) {
 		client.GetCitiesByCountry({'CountryName': CountryName}, function(err, result, body) {
-			callback(result);
+			//console.log(result.GetCitiesByCountryResult);
+			if (typeof result.GetCitiesByCountryResult != 'undefined') {
+				xml2js.parseString(result.GetCitiesByCountryResult, {"explicitArray": false }, function(errXml, resultXml) {
+					//console.log(resultXml.NewDataSet.Table);
+					callback(resultXml.NewDataSet.Table);
+				});
+			} else {
+				callback(null);
+			}
 		}, proxy);
 	}
 	
 	requestWeather = function(CityName, CountryName, callback) {
-		client.GetCitiesByCountry({'CityName': CityName, 'CountryName': CountryName}, function(err, result, body) {
-			callback(result);
+		client.GetWeather({'CityName': CityName, 'CountryName': CountryName}, function(err, result, body) {			
+			//console.log(result);
+			xml2js.parseString(result.GetWeatherResult, {"explicitArray": false }, function(errXml, resultXml) {
+				//console.log(resultXml);
+				if (typeof resultXml != 'undefined') {
+					//console.log(resultXml.CurrentWeather);
+					callback(resultXml.CurrentWeather);
+				} else {
+					callback(null);
+				}
+			});
 		}, proxy);
 	}
 	
@@ -62,16 +82,69 @@ app.configure(function() {
 });
 
 app.get('/stock/:param', function(req, res) {
-	// Cek dulu apakah variabel requestStockQuote apa masih null, kalau iya kasih pesan error dulu (SOAP nya berarti belum ke load pas pertama kali load skrip ini
-    res.render('viewStock.ejs')
+	if (requestStockQuote == null) {
+		return;
+	}
+	
+	var hasil = requestStockQuote(req.params.param, function(result) {
+		//console.log(result);
+		res.render('viewStock.ejs', {'result': result});
+	});
+	
 });
 
-app.get('/currency', function(req, res) {
-    res.render('viewCurrency.ejs')
+app.get('/currency/:from/:to', function(req, res) {
+	if (requestConversionRate == null) {
+		return;
+	}
+	
+	var from  = req.params.from.toUpperCase();
+	var to  = req.params.to.toUpperCase();
+	var hasil = requestConversionRate(from, to, function(result) {
+		//console.log(result);
+		result_array = {
+			from: from,
+			to: to,
+			value: result
+		};
+		res.render('viewCurrency.ejs', {'result': result_array});
+	});
+	
 });
 
-app.get('/cuaca', function(req, res) {
-    res.render('viewCuaca.ejs')
+app.get('/cuaca/:negara', function(req, res) {
+	if (requestCitiesByCountry == null) {
+		return;
+	}
+	
+	var negara  = req.params.negara;
+	var hasil = requestCitiesByCountry(negara, function(result) {
+		//console.log(result);
+		if (result != null) {
+			res.render('viewKota.ejs', {'result': result});
+		} else {
+			res.render('viewError.ejs');
+		}
+	});
+	
+});
+
+app.get('/cuaca/:negara/:kota', function(req, res) {
+	if (requestWeather == null) {
+		return;
+	}
+	
+	var negara  = req.params.negara;
+	var kota  = req.params.kota;
+	var hasil = requestWeather(kota, negara, function(result) {
+		//console.log(result);
+		if (result != null) {
+			res.render('viewCuaca.ejs', {'result': result});
+		} else {
+			res.render('viewError.ejs');
+		}
+	});
+	
 });
 
 app.listen(3000);
