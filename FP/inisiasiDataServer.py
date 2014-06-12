d7112ebf2437e54ccf0328c0a69d9b7c542ee214
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-06-13 00:08:22 +0700

    Tambah komentalalala

--- FP/inisiasiDataServer.py
+++ FP/inisiasiDataServer.py
@@ -4,6 +4,8 @@ client = MongoClient()
 db = client.fpsister
 
 dapatData = []
+
+# Ganti nama file dengan data 10% nya contoh jika tidak ingin leletletletletletlettelgatel
 for line in open('./kddcup.data.corrected'):
     hasil = line.strip().split(',')
     if len(dapatData)==100000:
