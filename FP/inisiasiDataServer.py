95c806fc2a12e2151524e3eac15b61aec6345df0
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-06-13 20:29:58 +0700

    Implementasi pemmbaca centroid

--- FP/inisiasiDataServer.py
+++ FP/inisiasiDataServer.py
@@ -1,16 +1,41 @@
 from pymongo import MongoClient
+import numpy
+from scipy.cluster.vq import kmeans
+
 
 client = MongoClient()
-db = client.fpsister
+## PROSES DATASET
+def prosesDataset():
+    db = client.fpsister
 
-dapatData = []
+    dapatData = []
+    #for line in open('./kddcup.data.corrected.delcol'):
+    for line in open('./kddcup.newtestdata_10_percent_unlabeled.delcol'):
+        hasil = line.strip().split(',')
+        if len(dapatData)==100000:
+            db.dataset.insert(dapatData)
+            dapatData = []
+        else:
+            dapatData.append({'data': hasil})
 
-# Ganti nama file dengan data 10% nya contoh jika tidak ingin leletletletletletlettelgatel
-for line in open('./kddcup.data.corrected'):
-    hasil = line.strip().split(',')
-    if len(dapatData)==100000:
-        db.dataset.insert(dapatData)
-        dapatData = []
-    else:
-        dapatData.append({'data': hasil})
+    db.dataset.create_index('occupiedBy')
+    db.dataset.create_index('data')
 
+## PROSES CENTROID
+def prosesCentroid():
+    db = client.fpsister
+    data = []
+    for line in open('./kddcup.newtestdata_10_percent_unlabeled.delcol'):
+        # Read whole data
+        row = line.strip().split(',')
+        row = map(float, row)
+        data.append(row)
+
+    centroid,label = kmeans(numpy.array(data), 23)
+    for row in centroid:
+        print row
+        db.centroid.insert({'data': map(str, row)})
+
+
+#prosesDataset()
+prosesCentroid()
